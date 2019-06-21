9cf560be1cb24aef3d68c6c72aae3408d0f5157d
SON NGUYEN <sonnguyen@SONs-Mac-mini.local>
2019-06-21 10:23:16 +0700

    update

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
+const lo = require('lodash');
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
@@ -18,9 +19,8 @@ app.get('/api/getList', (req,res) => {
     .then(result => {
         const dataLocal = result[0];
         const dataAPI = result[1];
-        console.log(typeof(dataLocal))
-        console.log(typeof(dataAPI.data))
-        res.send([...dataAPI.data, ...dataLocal])
+        data = lo.merge(dataAPI, dataLocal)
+        res.send(data.data)
     })
     .catch(error => {
         console.log(error)
@@ -32,9 +32,8 @@ const readVersionsFromFile = async() => {
     return new Promise((resolve, reject) => {
         fs.readFile('build-version.json', (err, data) => {  
             if (err) throw err;
-            
-            const dataLocal = JSON.parse(data);
-            resolve(dataLocal.data);
+            const dataLocal = JSON.parse(data) || {};
+            resolve(dataLocal);
         })
     });
 };
